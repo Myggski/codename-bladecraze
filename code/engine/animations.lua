198210d3d3384a6c9c0bf3037e211547be700342
Sodaro <dadda121@gmail.com>
2022-07-29 16:09:16 +0200

    animations, cleanup, player  creation refactoring

--- code/engine/animations.lua
+++ code/engine/animations.lua
@@ -1,25 +1,25 @@
 
 local animations = {}
 
-function animations.newAnimation(data)
+function animations.newAnimation(image, image_data, duration)
+  local offset_x, offset_y, width, height, frame_count = unpack(image_data)
+  local animation = {}
+  animation.quads = {};
+  animation.sprite_sheet = image;
+  animation.duration = duration
+  animation.current_time = 0
 
-    local animation = {}
-    animation.quads = {};
-    animation.spriteSheet = data.image;
-    animation.duration = data.duration
-    animation.currentTime = 0
-
-    local i = 0;
-    for y = data.offsetY, data.image:getHeight() - data.height, data.height do
-        for x = data.offsetX, data.image:getWidth() - data.width, data.width do
-            table.insert(animation.quads, love.graphics.newQuad(x, y, data.width, data.height, data.image:getDimensions()))
-            i = i + 1
-            if (i >= data.frameCount) then
-                return animation
-            end
-        end
-    end
-    return animation
+  local i = 0;
+  for y = offset_y, image:getHeight() - height, height do
+      for x = offset_x, image:getWidth() - width, width do
+          table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+          i = i + 1
+          if (i >= frame_count) then
+              return animation
+          end
+      end
+  end
+  return animation
 end
 
 return animations
--- code/player/player.lua
+++ code/player/player.lua
@@ -1,36 +1,87 @@
 Player = {}
-require("code.player.player_drawing")
 
-local Input = require("code.player.player_input")
+local rectangle = require("code.engine.rectangle")
+local player_drawing = require("code.player.player_drawing")
+
+require("code.player.player_input")
+local animations = require("code.engine.animations")
+
+local player_animations = {
+  {
+    idle_animation = {128,36,16,28,4},
+    run_animation = {192,36,16,28,4},
+    hit_animation = {256,36,16,28,1},
+  },
+  {
+    idle_animation = {128,68,16,28,4},
+    run_animation = {192,68,16,28,4},
+    hit_animation = {256,68,16,28,1},
+  },
+  {
+    idle_animation = {128,228,16,28,4},
+    run_animation = {192,228,16,28,4},
+    hit_animation = {256,228,16,28,1},
+  },
+  {
+    idle_animation = {128,164,16,28,4},
+    run_animation = {192,164,16,28,4},
+    hit_animation = {256,164,16,28,1},
+  },
+}
 
 function Player:update(dt)
-    --Update animation
-    self.animation.currentTime = self.animation.currentTime + dt
-    if self.animation.currentTime >= self.animation.duration then
-        self.animation.currentTime = self.animation.currentTime - self.animation.duration
-    end
-    --Move player
-    self.input = Input:get_input(self.index)
+  self.input = player_input:get_input(self.index)
 
-    self.x = self.x + self.input.x
-    self.y = self.y + self.input.y
-    self.box.x = self.x
-    self.box.y = self.y
+  --change between idle and run animations
+  if (self.input.x ~= 0 or self.input.y ~= 0) then
+    self.current_animation = self.run_animation
+  else
+    self.current_animation = self.idle_animation
+  end
+
+  --Update animation
+  self.current_animation.current_time = self.current_animation.current_time + dt
+  if self.current_animation.current_time > self.current_animation.duration then
+      self.current_animation.current_time = self.current_animation.current_time - self.current_animation.duration
+  end
+
+
+  --Move player
+  self.box.x = self.box.x + self.input.x
+  self.box.y = self.box.y + self.input.y
 end
 
-function Player:create(x, y, index, animation, box)
-    self.__index = self
-    local obj = setmetatable({
-        x = x,
-        y = y,
-        index = index,
-        input = {},
-        animation = animation,
-        box = box,
-        color = {1,1,1,1}
-    }, self)
-    
-    return obj
+function Player:create(data)
+  self.__index = self
+
+  local idle_animation = animations.newAnimation(
+    data.image,
+    player_animations[data.index].idle_animation,
+    1
+  )
+  local run_animation = animations.newAnimation(
+    data.image,
+    player_animations[data.index].run_animation,
+    0.5
+  )
+  local hit_animation = animations.newAnimation(
+    data.image,
+    player_animations[data.index].hit_animation,
+    2
+  )
+
+  local obj = setmetatable({
+      index = data.index,
+      idle_animation = idle_animation,
+      run_animation = run_animation,
+      hit_animation = hit_animation,
+      current_animation = idle_animation,
+      box = rectangle:create2(data.position, data.bounds), --x, y, w ,h 
+      color = {1,1,1,1},
+      input = {},
+  }, self)
+
+  return obj
 end
 
 return Player
