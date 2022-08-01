69ee84115c1a15bda1da3268a56d6d64d1e4df3b
Sodaro <dadda121@gmail.com>
2022-08-01 16:56:11 +0200

    spatial_grid and set wip

---
+++ code/utilities/set.lua
@@ -0,0 +1,54 @@
+set = {}
+    
+function set.create (t)
+  local res = {}
+  for _, l in ipairs(t) do res[l] = true end
+  return res
+end
+
+function set.union (a,b)
+  local res = set.new{}
+  for k in pairs(a) do res[k] = true end
+  for k in pairs(b) do res[k] = true end
+  return res
+end
+
+function set.add(in_set, key)
+  if not set.contains(in_set, key) then
+    in_set[key] = true
+  end
+end
+
+function set.intersection (a,b)
+  local res = set.new{}
+  for k in pairs(a) do
+    res[k] = b[k]
+  end
+  return res
+end
+
+function set.delete(in_set, key)
+  if in_set[key] ~= nil then
+    in_set[key] = nil
+  end
+end
+
+function set.tostring (in_set)
+  local s = "{"
+  local sep = ""
+  for e in pairs(in_set) do
+    s = s .. sep .. e
+    sep = ", "
+  end
+  return s .. "}"
+end
+
+function set.print (s)
+  print(set.tostring(s))
+end
+
+function set.contains (in_set, data)
+  return in_set[data] ~= nil
+end
+
+return set
