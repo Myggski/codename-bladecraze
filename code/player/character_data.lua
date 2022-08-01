086cd404ad6381689ca42a8c21ab7912c0cd9cd1
Sodaro <dadda121@gmail.com>
2022-08-01 10:18:17 +0200

    more player cleanup

---
+++ code/player/character_data.lua
@@ -0,0 +1,48 @@
+local character_data = {
+  elf = {
+    name = "Elven Ranger",
+    stats = {
+      strength = 0,
+      intelligence = 0,
+      agility = 2
+    },
+    idle_animation = { 128, 36, 16, 28, 4 },
+    run_animation = { 192, 36, 16, 28, 4 },
+    hit_animation = { 256, 36, 16, 28, 1 },
+  },
+  knight = {
+    name = "The Tank",
+    stats = {
+      strength = 2,
+      intelligence = 0,
+      agility = 0
+    },
+    idle_animation = { 128, 68, 16, 28, 4 },
+    run_animation = { 192, 68, 16, 28, 4 },
+    hit_animation = { 256, 68, 16, 28, 1 },
+  },
+  lizard = {
+    name = "Dragon",
+    stats = {
+      strength = 1,
+      intelligence = 0,
+      agility = 1
+    },
+    idle_animation = { 128, 228, 16, 28, 4 },
+    run_animation = { 192, 228, 16, 28, 4 },
+    hit_animation = { 256, 228, 16, 28, 1 },
+  },
+  wizard = {
+    name = "Arcanist",
+    stats = {
+      strength = 0,
+      intelligence = 2,
+      agility = 0
+    },
+    idle_animation = { 128, 164, 16, 28, 4 },
+    run_animation = { 192, 164, 16, 28, 4 },
+    hit_animation = { 256, 164, 16, 28, 1 },
+  },
+}
+
+return character_data
--- main.lua
+++ main.lua
@@ -12,19 +12,20 @@ function love.load()
   game_event_manager:invoke(GAME_EVENT_TYPES.LOAD)
 
   sprite_sheet_image = love.graphics.newImage("assets/0x72_DungeonTilesetII_v1.4.png")
-  
+  local characters = { "elf", "knight", "lizard", "wizard" }
   --Create 4 players
   players = {}
-  for i=1,4 do
-    local position = {i * 100, 200}
-    local bounds = {16, 28}
-    
-	  players[i] = player_character:create
+  for i = 1, 4 do
+    local position = { i * 100, 200 }
+    local bounds = { 16, 28 }
+
+    players[i] = player_character:create
     {
       image = sprite_sheet_image,
       position = position,
       bounds = bounds,
       index = i,
+      character = characters[i]
     }
   end
 end
@@ -32,16 +33,16 @@ end
 function love.update(dt)
   game_event_manager:invoke(GAME_EVENT_TYPES.UPDATE, dt)
   local x, y = love.mouse.getPosition()
-  for i=1, #players do
-	  local player, rect = players[i], players[i].box
-	  player:update(dt)
+  for i = 1, #players do
+    local player, rect = players[i], players[i].box
+    player:update(dt)
   end
 end
 
 function love.draw()
   game_event_manager:invoke(GAME_EVENT_TYPES.DRAW)
-  for i=1, #players do
-	  players[i]:draw()
+  for i = 1, #players do
+    players[i]:draw()
   end
 end
 
@@ -53,7 +54,7 @@ function love.mousereleased(x, y, btn, is_touch, pressed)
   game_event_manager:invoke(GAME_EVENT_TYPES.MOUSE_RELEASED, x, y, btn, is_touch, pressed)
 end
 
--- Can be stopped by returning true instead
+--Can be stopped by returning true instead
 function love.quit()
   local ready_to_quit = false
   game_event_manager:invoke(GAME_EVENT_TYPES.QUIT, ready_to_quit)
