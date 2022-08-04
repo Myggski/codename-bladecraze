2b2f489016d01adf1d6b2f487373fbdf1b0e26ee
Sodaro <dadda121@gmail.com>
2022-08-04 16:00:27 +0200

    grid and set cleanup

--- code/engine/spatial_grid.lua
+++ code/engine/spatial_grid.lua
@@ -1,6 +1,6 @@
-local spatial_grid = {}
-
 require("code.utilities.set")
+
+local spatial_grid = {}
 
 function spatial_grid:create(bounds, dimensions)
     self.__index = self
@@ -15,83 +15,89 @@ local function hash_key(x, y)
     return (x .. "." .. y)
 end
 
-function spatial_grid:new_client(position, dimensions)
+function spatial_grid:new_client(position, dimensions, guid)
     local client = {
         position = position,
         dimensions = dimensions,
+        guid = guid,
         indices = nil
     }
     self:insert(client)
     return client
 end
 
-function spatial_grid:get_cell_index(position)
-    local x = math.clamp01((position.x - self.bounds.x_min) / (self.bounds.x_max - self.bounds.x_min))
-    local y = math.clamp01((position.y - self.bounds.y_min) / (self.bounds.y_max - self.bounds.y_min))
-    local xIndex = math.floor(x * self.dimensions.w)
-    local yIndex = math.floor(y * self.dimensions.h)
+--[[
+    get the player position between 0.0-1.0 of the bounds
+    and return the matching cell indices
+]]
+function spatial_grid:get_cell_index(x, y)
+
+    x = math.clamp01(x / self.bounds.x_max)
+    y = math.clamp01(y / self.bounds.y_max)
+    local xIndex = math.floor(x * self.dimensions.w) + 1
+    local yIndex = math.floor(y * self.dimensions.h) + 1
     return xIndex, yIndex
 end
 
 function spatial_grid:draw()
-    for y=1, grid.bounds.y_max, cell_height do
-        for x=1, grid.bounds.x_max, cell_width do
-          love.graphics.rectangle("line", x, y, cell_width, cell_height)
+    for y = 1, grid.bounds.y_max, cell_height do
+        for x = 1, grid.bounds.x_max, cell_width do
+            love.graphics.rectangle("line", x, y, cell_width, cell_height)
         end
     end
 end
 
+--[[
+    insert the client into every cell that it occupies
+]]
 function spatial_grid:insert(client)
-    local x1, y1 = client.position.x, client.position.y
-    local w, h = client.dimensions.w, client.dimensions.h
 
-    --local position1 = {x = x1 - w / 2, y = y1 - h / 2}
-    local position1 = {x = x1, y = y1}
-    local min_x_index, min_y_index = self:get_cell_index(position1)
+    local x, y = client.position.x, client.position.y
+    local half_w, half_h = client.dimensions.w / 2, client.dimensions.h / 2
 
-    --local position2 = { x = x1 + w / 2, y = y1 + h / 2 }
-    local position2 = { x = x1 + w, y = y1 + h }
-    local max_x_index, max_y_index = self:get_cell_index(position2)
+    local min_x_index, min_y_index = self:get_cell_index(x - half_w, y - half_h)
+    local max_x_index, max_y_index = self:get_cell_index(x + half_w, y + half_h)
 
     client.indices = { min_x_index, min_y_index, max_x_index, max_y_index }
-    
+
     for i = min_x_index, max_x_index do
         for j = min_y_index, max_y_index do
             local key = hash_key(i, j)
-            
+
             if not set.contains(self.cells, key) then
-                self.cells[key] = {} 
+                self.cells[key] = {}
             end
             table.insert(self.cells[key], client)
         end
     end
 end
 
-function spatial_grid:find_near(position, bounds)
-    local x1, y1 = position.x, position.y
-    local w, h = bounds.w, bounds.h
+--[[
+    check all the cells that the client occupies
+    and return all the other clients that occupy the same 
+]]
+function spatial_grid:find_near(position, bounds, exclude_guids)
+    local x, y = position.x, position.y
+    local half_w, half_h = bounds.w / 2, bounds.h / 2
 
-    --local position1 = {x = x1 - w / 2, y = y1 - h / 2}
-    local position1 = {x = x1, y = y1}
-    local min_x_index, min_y_index = self:get_cell_index(position1)
+    local min_x_index, min_y_index = self:get_cell_index(x - half_w, y - half_h)
+    local max_x_index, max_y_index = self:get_cell_index(x + half_w, y + half_h)
 
-    --local position2 = { x = x1 + w / 2, y = y1 + h / 2 }
-    local position2 = { x = x1 + w, y = y1 + h}
-    local max_x_index, max_y_index = self:get_cell_index(position2)
+    local clients_set = {}
 
-    local clients = {} --set
-    
     for i = min_x_index, max_x_index do
         for j = min_y_index, max_y_index do
             local key = hash_key(i, j)
             if set.contains(self.cells, key) then
                 for k, v in ipairs(self.cells[key]) do
-                    set.add(clients, v)
+                    if not set.contains(exclude_guids, v.guid) then
+                        set.add(clients_set, v)
+                    end
                 end
             end
         end
     end
-    return clients
+    return clients_set
 end
 
 function spatial_grid:update(client)
@@ -99,6 +105,9 @@ function spatial_grid:update(client)
     self:insert(client)
 end
 
+--[[
+    remove the client from every cell that contains it
+]]
 function spatial_grid:remove_client(client)
     local min_x_index, min_y_index = client.indices[1], client.indices[2]
     local max_x_index, max_y_index = client.indices[3], client.indices[4]
@@ -106,7 +115,7 @@ function spatial_grid:remove_client(client)
     for i = min_x_index, max_x_index do
         for j = min_y_index, max_y_index do
             local key = hash_key(i, j)
-            if (set.contains(self.cells, key)) then
+            if set.contains(self.cells, key) then
                 local index = table.index_of(self.cells[key], client)
                 table.remove(self.cells[key], index)
             end
