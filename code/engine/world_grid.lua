c32d29361e8afeafa0c1f440456646032159a77b
David Bång <dadda121@gmail.com>
2022-09-05 21:30:47 +0200

    other grid convert vararg too

--- code/engine/world_grid.lua
+++ code/engine/world_grid.lua
@@ -12,8 +12,12 @@ function world_grid:convert_to_grid(world)
   return world / self.unit_size
 end
 
-function world_grid:world_to_grid(world_x, world_y)
-  return self:convert_to_grid(world_x), self:convert_to_grid(world_y)
+function world_grid:world_to_grid(...)
+  local args = { ... }
+  for key, value in ipairs(args) do
+    args[key] = self:convert_to_grid(value)
+  end
+  return unpack(args)
 end
 
 function world_grid:grid_to_world(...)
