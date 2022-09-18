daec267c294758ba4db9e7b4c20d9f76136d7593
Tomas Wallin <Myggski@users.noreply.github.com>
2022-09-18 12:37:10 +0200

    Finished all the tests for camera

--- spec/engine/camera/camera_spec.lua
+++ spec/engine/camera/camera_spec.lua
@@ -1,4 +1,9 @@
 require "spec.love_setup"
+local rectangle = require "code.engine.rectangle"
+local world_grid = require "code.engine.world_grid"
+
+local ZOOM_MAX = -3
+local ZOOM_MIN = 0
 
 insulate("camera", function()
   local camera = require "code.engine.camera.camera"
@@ -18,7 +23,7 @@ insulate("camera", function()
   end)
 
   describe("get_screen_game_size", function()
-    describe("when scale is set to five", function()
+    describe("when scale has default value (5)", function()
       describe("and zoom is set to zero", function()
         it("should return 1280 and 720 divided by five", function()
           local w, h = camera:get_screen_game_size()
@@ -47,7 +52,7 @@ insulate("camera", function()
       get_screen_game_size_spy = spy.on(camera, "get_screen_game_size")
     end)
 
-    describe("when scale is set to five", function()
+    describe("when scale has default value (5)", function()
       describe("and zoom is set to zero", function()
         it("should return 640 and 360 divided by five", function()
           local w, h = camera:get_screen_game_half_size()
@@ -298,6 +303,308 @@ insulate("camera", function()
         assert.stub(love.window.setFullscreen).was.called_with(false, "desktop")
         assert.is.falsy(camera.is_fullscreen)
       end)
+    end)
+  end)
+
+  describe("look_at", function()
+    describe("when the parameters x and y has values set", function()
+      it("should set cameras x and y to the same value", function()
+        camera:look_at(128, 32)
+        assert.is:truthy(camera.x == 128, camera.y == 32)
+
+        camera:look_at(-32, -128)
+        assert.is:truthy(camera.x == -32, camera.y == -128)
+
+        camera:look_at(0, 0)
+        assert.is:truthy(camera.x == 0, camera.y == 0)
+
+        camera:look_at("Hello", "World!")
+        assert.is:truthy(camera.x == "Hello", camera.y == "World!")
+
+        camera:look_at("", "")
+        assert.is:truthy(camera.x == "", camera.y == "")
+      end)
+
+      describe("when the parameters x and y does not have any values", function()
+        it('should set cameras x and y to 0', function()
+          camera.x, camera.y = 128, 32
+
+          camera:look_at()
+          assert.is:truthy(camera.x == 0, camera.y == 0)
+        end)
+      end)
+    end)
+
+    describe("is_outside_camera_view", function()
+      describe("when the rectangle top-left is in the center of the screen", function()
+        it("should not be outside of camera view", function()
+          local rect = rectangle(0, 0, 1, 1)
+
+          assert.is.falsy(camera:is_outside_camera_view(rect))
+        end)
+      end)
+
+      describe("when the rectangles top-left is in the bottom-right corner of the screen", function()
+        it("should be outside of camera view", function()
+          local world_x, world_y = world_grid:world_to_grid(camera:get_screen_game_half_size())
+          local rect = rectangle(world_x, world_y, 1, 1)
+
+          assert.is.truthy(camera:is_outside_camera_view(rect))
+        end)
+      end)
+
+      describe("when the rectangles top-left is in the top-left corner of the screen", function()
+        it("should not be outside of camera view", function()
+          local world_x, world_y = world_grid:world_to_grid(camera:get_screen_game_half_size())
+          local rect = rectangle(-world_x, -world_y, 1, 1)
+
+          assert.is.falsy(camera:is_outside_camera_view(rect))
+        end)
+      end)
+    end)
+  end)
+
+  describe("start_draw_world", function()
+    it('should setup the canvas_game, clear it and center position the camera view', function()
+      stub(love.graphics, "setCanvas")
+      stub(love.graphics, "clear")
+      stub(love.graphics, "push")
+      stub(love.graphics, "translate")
+      camera:look_at(world_grid:world_to_grid(16, 32))
+
+      camera:start_draw_world()
+
+      local half_width, half_height = camera:get_screen_game_half_size()
+      assert.stub(love.graphics.setCanvas).was.called(1)
+      assert.stub(love.graphics.setCanvas).was.called_with(camera.canvas_game)
+      assert.stub(love.graphics.clear).was.called(1)
+      assert.stub(love.graphics.clear).was.called_with(0, 0, 0, 0)
+      assert.stub(love.graphics.push).was.called(1)
+      assert.stub(love.graphics.translate).was.called(2)
+      assert.stub(love.graphics.translate).was.called_with(math.round(half_width), math.round(half_height))
+      assert.stub(love.graphics.translate).was.called_with(-16, -32)
+    end)
+  end)
+
+  describe("stop_draw_world", function()
+    describe("when scale and zoom has default values (5 and 0)", function()
+      it("should draw the canvas_game and reverse the push operation with default scale value", function()
+        stub(love.graphics, "setCanvas")
+        stub(love.graphics, "pop")
+        stub(love.graphics, "draw")
+
+        camera:stop_draw_world()
+
+        assert.stub(love.graphics.setCanvas).was.called(1)
+        assert.stub(love.graphics.pop).was.called(1)
+        assert.is.truthy(camera.scale + camera.zoom == default_scale)
+        assert.stub(love.graphics.draw).was.called_with(camera.canvas_game, 0, 0, 0, camera.scale + camera.zoom)
+      end)
+    end)
+  end)
+
+  describe("when scale has default value (5) but zoom is set to -1", function()
+    it("should draw the canvas_game and reverse the push operation with 4 in scale", function()
+      stub(love.graphics, "setCanvas")
+      stub(love.graphics, "pop")
+      stub(love.graphics, "draw")
+
+      camera:set_zoom(-1)
+      camera:stop_draw_world()
+
+      assert.stub(love.graphics.setCanvas).was.called(1)
+      assert.stub(love.graphics.pop).was.called(1)
+      assert.is.truthy(camera.scale + camera.zoom == 4)
+      assert.stub(love.graphics.draw).was.called_with(camera.canvas_game, 0, 0, 0, camera.scale + camera.zoom)
+    end)
+  end)
+
+  describe("start_draw_hud", function()
+    it("should setup the canvas_hud and clear it", function()
+      stub(love.graphics, "setCanvas")
+      stub(love.graphics, "clear")
+
+      camera:start_draw_hud()
+
+      assert.stub(love.graphics.setCanvas).was.called(1)
+      assert.stub(love.graphics.setCanvas).was.called_with(camera.canvas_hud)
+      assert.stub(love.graphics.clear).was.called(1)
+      assert.stub(love.graphics.clear).was.called_with(0, 0, 0, 0)
+    end)
+  end)
+
+  describe("stop_draw_hud", function()
+    it("should draw the canvas_hud", function()
+      stub(love.graphics, "setCanvas")
+      stub(love.graphics, "draw")
+
+      camera:stop_draw_hud()
+
+      assert.stub(love.graphics.setCanvas).was.called(1)
+      assert.stub(love.graphics.setCanvas).was.called_with()
+      assert.stub(love.graphics.draw).was.called(1)
+      assert.stub(love.graphics.draw).was.called_with(camera.canvas_hud)
+    end)
+  end)
+
+  describe("get_scale", function()
+    it("should return cameras scale value", function()
+      assert.is.truthy(camera:get_scale() == default_scale)
+
+      camera.scale = 1
+      assert.is.truthy(camera:get_scale() == 1)
+
+      camera.scale = -13
+      assert.is.truthy(camera:get_scale() == -13)
+
+      camera.scale = "donut"
+      assert.is.truthy(camera:get_scale() == "donut")
+
+      camera.scale = nil
+      assert.is.truthy(camera:get_scale() == 0)
+    end)
+  end)
+
+  describe("get_zoom", function()
+    it("should return cameras zoom value", function()
+      assert.is.truthy(camera:get_zoom() == default_zoom)
+
+      camera.zoom = 1
+      assert.is.truthy(camera:get_zoom() == 1)
+
+      camera.zoom = -13
+      assert.is.truthy(camera:get_zoom() == -13)
+
+      camera.zoom = "donut"
+      assert.is.truthy(camera:get_zoom() == "donut")
+
+      camera.zoom = nil
+      assert.is.truthy(camera:get_zoom() == 0)
+    end)
+  end)
+
+  describe("set_zoom", function()
+    describe("when zoom is set to a higher value than 0", function()
+      it("should set the zoom to 0", function()
+        camera:set_zoom(0.01)
+        assert.is.truthy(camera:get_zoom() == ZOOM_MIN)
+
+        camera.zoom = 0
+
+        camera:set_zoom(128)
+        assert.is.truthy(camera:get_zoom() == ZOOM_MIN)
+      end)
+    end)
+
+    describe("when zoom is set to a lower value than " .. ZOOM_MAX, function()
+      it("should set the zoom to " .. ZOOM_MAX, function()
+        camera:set_zoom(ZOOM_MAX - 0.00001)
+        assert.is.truthy(camera:get_zoom() == ZOOM_MAX)
+
+        camera.zoom = 0
+
+        camera:set_zoom(-128)
+        assert.is.truthy(camera:get_zoom() == ZOOM_MAX)
+      end)
+    end)
+
+    describe("when zoom is set between 0 and " .. ZOOM_MAX, function()
+      it("should set the actual value for camera zoom", function()
+        camera:set_zoom(-0.25)
+        assert.is.truthy(camera:get_zoom() == -0.25)
+
+        camera.zoom = 0
+
+        camera:set_zoom(ZOOM_MAX + 0.000001)
+        assert.is.truthy(camera:get_zoom() == ZOOM_MAX + 0.000001)
+      end)
+    end)
+  end)
+
+  describe("can_zoom_out", function()
+    describe("when zoom is larger than " .. ZOOM_MAX, function()
+      it("should return true", function()
+        assert.is.truthy(camera:can_zoom_out())
+
+        camera.zoom = -1
+        assert.is.truthy(camera:can_zoom_out())
+
+        camera.zoom = -2
+        assert.is.truthy(camera:can_zoom_out())
+      end)
+    end)
+
+    describe("when zoom is equal to " .. ZOOM_MAX, function()
+      it("should return false", function()
+        camera.zoom = ZOOM_MAX
+        assert.is.falsy(camera:can_zoom_out())
+      end)
+    end)
+  end)
+
+  describe("can_zoom_in", function()
+    describe("when zoom is less than " .. ZOOM_MAX, function()
+      it("should return true", function()
+        camera.zoom = ZOOM_MAX
+        assert.is.truthy(camera:can_zoom_in())
+
+        camera.zoom = -1
+        assert.is.truthy(camera:can_zoom_in())
+
+        camera.zoom = -2
+        assert.is.truthy(camera:can_zoom_in())
+      end)
+    end)
+
+    describe("when zoom is equal to " .. ZOOM_MIN, function()
+      it("should return false", function()
+        assert.is.falsy(camera:can_zoom_in())
+      end)
+    end)
+  end)
+
+  describe("set_canvas_game", function()
+    it("should setup the canvas_game", function()
+      local stub_canvas = {}
+
+      stub(love.graphics, "newCanvas").returns(stub_canvas)
+      stub(stub_canvas, "setFilter")
+
+      camera:set_canvas_game(1280, 720)
+
+      assert.stub(love.graphics.newCanvas).was.called_with(1280, 720)
+      assert.stub(stub_canvas.setFilter).was.called_with(stub_canvas, "nearest", "nearest")
+      assert.are.equal(camera.canvas_game, stub_canvas)
+    end)
+  end)
+
+  describe("set_canvas_hud", function()
+    it("should setup the canvas_hud", function()
+      local stub_canvas = {}
+
+      stub(love.graphics, "newCanvas").returns(stub_canvas)
+      stub(stub_canvas, "setFilter")
+
+      camera:set_canvas_hud(1280, 720)
+
+      assert.stub(love.graphics.newCanvas).was.called_with(1280, 720)
+      assert.stub(stub_canvas.setFilter).was.called_with(stub_canvas, "nearest", "nearest")
+      assert.are.equal(stub_canvas, camera.canvas_hud)
+    end)
+  end)
+
+  describe("load", function()
+    it("should call set_canvas_game and set_canvas_hud", function()
+      local width, height = camera:get_screen_game_size()
+      local scaled_width, scaled_height = width * camera:get_scale(), height * camera:get_scale()
+
+      stub(camera, "set_canvas_game")
+      stub(camera, "set_canvas_hud")
+
+      camera:load()
+
+      assert.stub(camera.set_canvas_game).was.called_with(camera, width, height)
+      assert.stub(camera.set_canvas_hud).called_with(camera, scaled_width, scaled_height)
     end)
   end)
 end)
