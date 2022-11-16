e416b0dc2dd74aec5fd39db7fae7badf898beb47
Tomas Wallin <tomaswallin@hotmail.com>
2022-11-16 09:42:22 +0100

    added parameters iinstead of ...

--- code/engine/utilities.lua
+++ code/engine/utilities.lua
@@ -1,14 +1,11 @@
 local vector2 = require "code.engine.vector2"
 local gizmos = require "code.engine.debug.gizmos"
 
-local function is_inside(...)
-  local x, y, position, size = ...
+local function is_inside(x, y, position, size)
   return position.x <= x and position.y <= y and (position.x + size.x) >= x and (position.y + size.y) >= y
 end
 
-local function get_center_position(...)
-  local position, size = ...
-
+local function get_center_position(position, size)
   return vector2(position.x + (size.x * 0.5), position.y + (size.y * 0.5))
 end
 
