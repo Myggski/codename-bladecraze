0cd6400eb67335facf9c55115a757a02a54f705b
Tomas Wallin <Myggski@users.noreply.github.com>
2022-11-25 14:44:49 +0100

    updated powerups and fire logic

--- code/game/entities/powerups/powerup_data.lua
+++ code/game/entities/powerups/powerup_data.lua
@@ -1,21 +1,21 @@
 local data = {
   {
     name = "fire_upgrade",
-    animation_data = { 0, 0, 16, 24, 1 },
+    animation_data = { 0, 0, 16, 16, 1 },
     stats = {
       [UPGRADE_KEYS.BOMB_RADIUS] = 1,
     }
   },
   {
     name = "bomb_upgrade",
-    animation_data = { 0, 24, 16, 24, 1 },
+    animation_data = { 0, 16, 16, 16, 1 },
     stats = {
       [UPGRADE_KEYS.BOMBS] = 1,
     }
   },
   {
     name = "speed_upgrade",
-    animation_data = { 0, 48, 16, 24, 1 },
+    animation_data = { 0, 32, 16, 16, 1 },
     stats = {
       [UPGRADE_KEYS.SPEED] = 10
     }
